e0ad917449116b497b823739dc100dfa4c766ce2
Tarma <hujing316@aliyun.com>
2023-07-21 23:19:33 +0800

    框架搭建完毕

--- server/src/main/java/com/imooc/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/imooc/pan/server/RPanServerLauncher.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {RPanConstants.BASE_COMPONENT_SCAN_PATH})
 @ServletComponentScan(basePackages = {RPanConstants.BASE_COMPONENT_SCAN_PATH + ".web"})
